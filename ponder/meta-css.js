ac3d6c6da111264b9c87957aa616d24f6a379442
carlnelson275 <zachnelson275@gmail.com>
2025-09-20 19:38:43 -0600

    mission

---
+++ ponder/meta-css.js
@@ -0,0 +1,21 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = PI * radius**2;
+console.log(area);
+
+radius = 11;
+area = PI * radius**2;
+console.log(area);
+
+const ONE = 1;
+const TWO = '2';
+
+let course = "CSE131"; // global scope
+if (true) {
+    let student = "Zach";
+    console.log(course);  // works just fine, course is global
+    console.log(student); // works just fine, it's being accessed within the block
+}
+console.log(course); // works fine, course is global
+console.log(student); // does not work, can't access a block variable outside the block
