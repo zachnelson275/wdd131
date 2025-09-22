237497ced97ca8a4c402f17386e7dda7299fd705
carlnelson275 <zachnelson275@gmail.com>
2025-09-22 12:23:36 -0600

    javascript listener & html list selector

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -21,4 +21,11 @@ if (true) {
     console.log(student); // works just fine, it's being accessed within the block
 }
 console.log(course); // works fine, course is global
-console.log(student); // does not work, can't access a block variable outside the block
+// console.log(student); // does not work, can't access a block variable outside the block
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+})
