523f06c081359c97f147aa9e2869d7ac999688fc
Zach <zachnelson275@gmail.com>
2025-09-27 11:28:53 -0600

    fixed mission dark mode

--- mission/mission.js
+++ mission/mission.js
@@ -1,23 +1,23 @@
 let selectElem = document.querySelector('#theme-select');
-let pageContent = document.querySelector('#text');
+let pageContent = document.querySelector('body');
 let logoContent = document.querySelector('#logo');
-let subTextContent = document.querySelector('#subtext');
+let subTextContent = document.querySelector('#sub-text');
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
-    if (current == 'light') {
+    if (current === 'light') {
         logoContent.src = 'byui-logo.webp';
         logoContent.alt = 'BYU-I Logo';
         pageContent.style.backgroundColor = 'white';
         pageContent.style.color = 'black';
         subTextContent.style.color = '#035f9c';
-    } else if (current == 'dark') {
+    } else if (current === 'dark') {
         logoContent.src = 'dark-logo.png';
         logoContent.alt = 'BYU-I Dark Logo';
-        pageContent.style.backgroundColor = 'darkgray';
+        pageContent.style.backgroundColor = '#242424ff';
         pageContent.style.color = 'white';
-        subTextContent.style.color = 'lightblue';
+        subTextContent.style.color = '#3597d8ff';
     }
 }
