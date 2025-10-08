763e69efedb21aa1d56283571598b2ec7e062dc2
Zach <zachnelson275@gmail.com>
2025-10-08 13:56:45 -0600

    scripture finished

---
+++ ponder/scripture/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".menu-btn");
+const menu = document.querySelector("nav");
+
+btn.addEventListenerq("click", toggleMenu());
+
+function toggleMenu() {
+    menu.classList.toggle("hide");
+    btn.classList.toggle("change");
+}
