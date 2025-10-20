5729ab59bde34eff4de4f6be3997f6ca4c759c3d
carlnelson275 <zachnelson275@gmail.com>
2025-10-20 11:59:03 -0600

    created arrays webpage

---
+++ ponder/arrays/arrays.js
@@ -0,0 +1,30 @@
+function convert(grade) {
+    switch (grade){
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('not a valid grade');
+    }
+    return points;
+}
+
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+const students = [
+    {last: 'Andrus', first: 'Aaron'},
+    {last: 'Masa', first:'Manny'},
+    {last: 'Tanda', first: 'Tamanda'}
+];
