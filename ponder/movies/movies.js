6200eb51a9c1399761e67b2164a8b961d7255727
carlnelson275 <zachnelson275@gmail.com>
2025-10-22 12:12:16 -0600

    dynamic aria tags

--- ponder/movies/movies.js
+++ ponder/movies/movies.js
@@ -50,7 +50,7 @@ const movies = [
                     <p><strong>Release Date:</strong> ${movie.date}</p>
                     <p><strong>Recommended Age:</strong> ${movie.ages}</p>
                     <p><strong>Genre:</strong> ${movie.genre}</p>
-                    <p><strong>Rating:</strong> <span aria-label="5 out of 5 stars" role="img">${movie.stars}</span></p>
+                    <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
                     <p id='desc'>${movie.description}</p>
                 </article>`;
 
