ece4701f28351d09557503743d8d9ea8bb7d29be
carlnelson275 <zachnelson275@gmail.com>
2025-11-03 11:38:15 -0700

    assignment setup

---
+++ ponder/objects/objects.js
@@ -0,0 +1,41 @@
+const aCourse = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'images/js-logo.png',
+    sections: [
+    { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+    { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+    ],
+    enrollStudent: function (sectionNum) {
+        // find the right section...Array.findIndex will work here
+        const sectionIndex = this.sections.findIndex(
+          (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+          this.sections[sectionIndex].enrolled++;
+          renderSections(this.sections);
+        }
+      }
+  };
+
+function sectionTemplate(section) {
+    return `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections) {
+const html = sections.map(sectionTemplate);
+document.querySelector("#sections").innerHTML = html.join("");
+}
+
+renderSections(aCourse.sections);
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+});
+          
